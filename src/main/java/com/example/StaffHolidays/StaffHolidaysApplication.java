7f29eb912dbefe5716fa91ad08e86ab13898005e
Pantarelo <118564862+Pantarelo@users.noreply.github.com>
2024-05-26 15:24:49 +0300

    first commit

---
+++ src/main/java/com/example/StaffHolidays/StaffHolidaysApplication.java
@@ -0,0 +1,13 @@
+package com.example.StaffHolidays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffHolidaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffHolidaysApplication.class, args);
+	}
+
+}
